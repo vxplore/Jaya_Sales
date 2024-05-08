bf43c9dd6873a61e8acb8bf062da83090e9c30cd
subhajitVxplore <subhajit@v-xplore.com>
2024-05-08 11:39:34 +0530

    versionCode = 5 versionName = "2.0.3"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.jayasales"
         minSdk = 27
         targetSdk = 34
-        versionCode = 4
-        versionName = "2.0.2"
+        versionCode = 5
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
